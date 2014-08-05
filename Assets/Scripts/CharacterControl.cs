735ebc2de6df7f4c531b6adf65c3102fb6e5e9eb
Aleksandra290709 <aleksandra290709@gmail.com>
2014-08-05 14:47:48 +0000

    Basic players logic added (by Alex & Sanja) Note: This is work in progress, code isn't cleanest possible. It'll be fixed.

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,10 +5,15 @@ public class CharacterControl : MonoBehaviour {
 
 	private CharacterController controller;
 	private CollisionFlags collisionFlags;
+	private BoardScript board;
+	public int playerNum;
+	private int numOfMoves;
+	public int discesSum;
 
 	// Use this for initialization
 	void Start () {
-	
+		board = gameObject.GetComponent<BoardScript> ();
+		numOfMoves = 0;
 	}
 	
 	// Update is called once per frame
@@ -30,15 +35,63 @@ public class CharacterControl : MonoBehaviour {
 
 		//		controller = gameObject.GetComponent<CharacterController> ();
 
-		if (Input.GetKeyDown (KeyCode.RightArrow))
-			transform.Translate (1, 0, 0);
-		else if (Input.GetKeyDown (KeyCode.LeftArrow))
-			transform.Translate (-1, 0, 0);
+		if (playerNum == 0)
+		{
+		int xPos, zPos;
+		xPos = board.playersPosition [playerNum].X;
+		zPos = board.playersPosition [playerNum].Z;
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			if (board.isValid (playerNum, xPos, zPos, xPos + 1, zPos)) 
+			{
+				board.playersPosition [playerNum].X += 1;
+				transform.Translate (-1, 0, 0);
+				//FIX ME
+				if(board.board[xPos,zPos] ==  10 || board.board[xPos+1,zPos] ==  10)
+					numOfMoves++;
+
+				Debug.Log("xPos: " + (xPos+1) + " zPos: " + zPos + "BROJ POTEZA: " + numOfMoves);
+			}
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			if (board.isValid (playerNum, xPos, zPos, xPos - 1, zPos)) 
+			{
+				board.playersPosition [playerNum].X -= 1;
+				transform.Translate (1, 0, 0);
+				//FIX ME
+				if(board.board[xPos,zPos] ==  10 || board.board[xPos-1,zPos] ==  10)
+					numOfMoves++;
+
+				Debug.Log("xPos: " + (xPos-1) + " zPos: " + zPos+ "BROJ POTEZA: " + numOfMoves);
+			}
+		}
 		else if (Input.GetKeyDown (KeyCode.UpArrow))
-			transform.Translate (0, 0, 1);
+		{			
+			if (board.isValid (playerNum, xPos, zPos, xPos, zPos + 1)) 
+			{
+				board.playersPosition [playerNum].Z += 1;
+				transform.Translate (0, 0, -1);
+				//FIX ME
+				if(board.board[xPos,zPos] ==  10 || board.board[xPos,zPos + 1] ==  10)
+					numOfMoves++;
+
+				Debug.Log("xPos: " + xPos + " zPos: " + (zPos + 1)+ "BROJ POTEZA: " + numOfMoves);
+			}
+		}
 		else if (Input.GetKeyDown (KeyCode.DownArrow))
-			transform.Translate (0, 0, -1);
+		{
+			if (board.isValid (playerNum, xPos, zPos, xPos, zPos - 1)) 
+			{
+				board.playersPosition [playerNum].Z -= 1;
+				transform.Translate (0, 0, 1);
+				//FIX ME
+				if(board.board[xPos,zPos] ==  10 || board.board[xPos,zPos-1] ==  10)
+					numOfMoves++;
 
+				Debug.Log("xPos: " + xPos + " zPos: " + (zPos - 1)+ "BROJ POTEZA: " + numOfMoves);
+			}
+		}
 
+		}
 	}
 }
