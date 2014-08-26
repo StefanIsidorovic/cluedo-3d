d971aa168e31ba159a052f6adf7fc1165878c2e1
zbetmen1 <zbetmen1@gmail.com>
2014-08-26 14:05:49 +0000

    Added some scripts for dice (they were here but weren't committed until now).

---
+++ Assets/Scripts/SideTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideTrigger : MonoBehaviour {
+
+	public int faceValue = 0;
+	
+	void OnTriggerEnter(Collider other ) {
+		
+		var dieGameObject = GameObject.Find("Dice1");
+		
+		var dieValueComponent = dieGameObject.GetComponent<DieValue>();
+		
+		dieValueComponent.currentValue = faceValue;
+		
+		Debug.Log(faceValue);
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
