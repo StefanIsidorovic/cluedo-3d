178875d9cafed62cbcb2aeed28f410a29a0908b6
mijalkovic.sanja@gmail.com <mijalkovic.sanja@gmail.com@2a9e148c-8d5c-3df3-e8d3-0f1a9be4f613>
2014-09-24 18:32:29 +0000

    Log corrected.

--- trunk/Assets/Scripts/GUIScript.cs
+++ trunk/Assets/Scripts/GUIScript.cs
@@ -917,7 +917,8 @@ public class GUIScript : MonoBehaviour
     private void GenerateMessageForAllPlayers()
     {
         PublicPlayerName = GameObject.Find("Player" + gameManager.OnTurn()).gameObject.GetComponent<CharacterControl>().PublicName();
-        PublicPlayerName = "<color=green>" + PublicPlayerName + "</color>";
+        if (!PublicPlayerName.StartsWith("<color=green>"))
+            PublicPlayerName = "<color=green>" + PublicPlayerName + "</color>";
         int dicesSum = GameObject.Find("GameManager").gameObject.GetComponent<GameManager>().DicesSum();
         if (dicesSum > 0 && askDialogShow == false && !askDialogShowQuestion && !askDialogShowCardsBool && !questionAsk && !infoBox)
         {
